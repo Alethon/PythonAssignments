b656810b2cc70908ee7758a6f119f5ce3dc437ab
Alethon <jacob.laster23@gmail.com>
2019-01-13 22:04:50 -0500

    first problem dome

---
+++ Prelab01/simpleTasks.py
@@ -0,0 +1,19 @@
+import os
+
+def find(pattern):
+	matches = []
+	with open(os.path.join(DataPath, 'sequence.txt'), "r") as file:
+		s = file.readline()
+	for i in range(len(s) - len(pattern) + 1):
+		discard = 0
+		for j in range(len(pattern)):
+			if not discard and str.isdigit(pattern[j]) and pattern[j] is not s[i:i+len(pattern)][j]:
+				discard = 1
+		if not discard:
+			matches.append(s[i:i+len(pattern)])
+	return matches
+
+if __name__ == "__main__":
+	DataPath = '/home/ecegridfs/a/ee364/DataFolder/Prelab01'
+	find("X01X")
+	
