1915bbc4cb598976cc2e9a46518d06e29c9c9aae
Alethon <jacob.laster23@gmail.com>
2019-01-20 23:34:29 -0500

    q2

--- Prelab02/ioTasks.py
+++ Prelab02/ioTasks.py
@@ -13,4 +13,31 @@ import sys
 DataPath = os.path.expanduser('~ee364/DataFolder/Prelab02')
 
 def fileList():
-    return
+    files = [f for f in os.listdir(DataPath) if os.path.isfile(os.path.join(DataPath, f))]
+    return files
+
+def getMaxDifference(symbol):
+    filename = os.path.join(DataPath, symbol + ".dat")
+    with open(filename, "r") as file:
+        data = [line.strip().split(',') for line in file.readlines()][2:]
+    maxDiff = 0
+    maxDate = data[0][0]
+    for date in data:
+        if float(date[4]) - float(date[5]) > maxDiff:
+            maxDiff = float(date[4]) - float(date[5])
+            maxDate = date[0]
+    return maxDate
+
+def getGainPercent(symbol):
+    filename = os.path.join(DataPath, symbol + ".dat")
+    with open(filename, "r") as file:
+        data = [line.strip().split(',') for line in file.readlines()][2:]
+    gain = 0
+    for date in data:
+        if float(date[1]) > float(date[3]):
+            gain += 1
+    return 100 * gain / len(data)
+
+if __name__ == "__main__":
+    #print(fileList())
+    print(getGainPercent("AAPL"))
